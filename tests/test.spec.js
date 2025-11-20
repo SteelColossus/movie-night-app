e7e2cddca60c151d4d23b442e9ad7c02167c9550
Michael Winkworth <michaelrwinkworth@gmail.com>
2025-11-20 23:37:42 +0000

    Add snapshots for each view

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -5,6 +5,7 @@ test('creates and finishes a movie night', async ({ page }) => {
     await expect(page).toHaveTitle('Movie Night App');
 
     await page.locator('#username').fill('User 1');
+    await expect(page).toHaveScreenshot('username-view.png');
     await page.locator('#username').press('Enter');
     await expect(page.locator('#usernameIndicator')).toHaveText('User 1');
 
@@ -12,21 +13,25 @@ test('creates and finishes a movie night', async ({ page }) => {
     await page.selectOption('#numSuggestions', { label: '1' });
 
     await page.locator('#nightName').fill('My Movie Night');
+    await expect(page).toHaveScreenshot('host-view.png');
     await page.locator('#nightName').press('Enter');
     await expect(page.locator('#movieNightTitle')).toHaveText('My Movie Night');
 
     await page.locator('#suggestion').fill('Harry Potter');
+    await expect(page).toHaveScreenshot('search-view.png');
     await page.locator('#suggestion').press('Enter');
 
     const movieRow = page.locator(
         '//table[@id="suggestionTable"]//tr[./td[text() = "Harry Potter and the Goblet of Fire"]]'
     );
     await expect(movieRow.locator('td:text("2005")')).toBeVisible();
+    await expect(page).toHaveScreenshot('suggestions-view.png');
     await movieRow.locator('input[type="button"]').click();
 
     await expect(
         page.locator('//table[@id="movieTable"]//tr/td[text() = "2 hours 37 mins"]')
     ).toBeVisible();
+    await expect(page).toHaveScreenshot('vote-view.png');
     await page.locator('#closeSuggestionsButton').click();
 
     const voteButton = page.locator('table#voteTable td > input[type="button"][value="Vote!"]');
@@ -45,7 +50,7 @@ test('creates and finishes a movie night', async ({ page }) => {
 
     await expect(page.locator('#winner')).toContainText('Harry Potter and the Goblet of Fire');
     await expect(page.locator('#voteChart')).toBeVisible();
-    await expect(page).toHaveScreenshot('after-results.png');
+    await expect(page).toHaveScreenshot('results-view.png');
     await page.locator('#endButton').click();
 
     await expect(page.locator('#nightName')).toBeVisible();
